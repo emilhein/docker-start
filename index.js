8f2088d24f77903f1886818422c0dbc15e601277
Emil Hein <emilhein1@gmail.com>
2017-10-24 22:14:27 +0200

    add postgress and sequalize to the project

--- index.js
+++ index.js
@@ -76,3 +76,16 @@ app.get('/runspread', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
 });
+
+const Sequelize = require('sequelize');
+
+// Or you can simply use a connection uri
+const sequelize = new Sequelize('postgres://someUser@postgres/myDB');
+sequelize
+.authenticate()
+.then(() => {
+    console.log('Connection has been established successfully.');
+})
+.catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
